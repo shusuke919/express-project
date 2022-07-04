4fe2d2989309a6cdb3cf9311ef84cee1ca6148f9
shusuke919 <sh091965@gmail.com>
2022-07-04 10:52:34 +0900

    終了

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { omikujiRouter } from "./routes/omikuji.route.js";
+import { jankenRouter } from "./routes/janken.route.js";
+
 const app = express();
+// ↓POSTでデータを受け取るために必要
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 const port = 3001;
 
 app.get("/", (req, res)=> {
@@ -10,32 +16,11 @@ app.get("/", (req, res)=> {
     });
 });
 
+
 app.use("/omikuji", (req, res) => omikujiRouter(req, res));
-app.get("/omikuji", (req, res) => {
-res.json({
-  uri: "/omikuji",
-  message: "おみくじです"
-});
-
-// app.get("/omikuji", (req, res) => {
-//   const omikuji = ["大吉", "中吉", "末吉", "小吉", "凶"];
-//   const min = 0;
-//   const max = omikuji.length - 1;
-//   const index = Math.floor(Math.random() * (max - min +1)) + min;
-// res.json({
-//   uri: "/omikuji",
-//   message: omikuji[index]
-// });
-
-});
-
-app.get("/janken", (req, res) => {
-  res.json({
-    uri: "/janken",
-    message: "これはじゃんけん"
-  });
+app.use("/janken", (req, res) => jankenRouter(req, res));
   
-  });
+
 
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
